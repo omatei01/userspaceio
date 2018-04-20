a35559afe41d70b553a3c5783bd5f45e5220cf07
sgoldsmith <sgoldsmith@dx4350>
2018-04-19 22:09:49 -0400

    test

--- libgpiod/python/src/buttoncallback.py
+++ libgpiod/python/src/buttoncallback.py
@@ -9,56 +9,35 @@ Should work on any board with a button built in. Just change chip and line
 value as needed.
 """
 
-import sys, time
+import sys, time, gpiod
 from argparse import *
-from cffi import FFI
-from libgpiod import libgpiod
 
 
 class buttoncallback:
     
-    def __init__(self):
+    def __init__(self, chip):
         """Create library and ffi interfaces.
         """         
-        self.gpiod = libgpiod.libgpiod()
-        self.lib = self.gpiod.lib
-        self.ffi = self.gpiod.ffi
+        self.chip = gpiod.Chip(chip, gpiod.Chip.OPEN_BY_PATH)
 
-        # Inner Callback function
-        @self.ffi.callback("int (int evtype, unsigned int offset, const struct timespec *ts, void *data)")
-        def buttonCallback(evtype, offset, ts, data):
-            # Use a try/except or an exception will cause an endless loop
-            try:
-                if evtype == self.lib.GPIOD_CTXLESS_EVENT_CB_TIMEOUT:
-                    rc = self.lib.GPIOD_CTXLESS_EVENT_CB_RET_STOP
-                    print("Timeout")
-                else:
-                    rc = self.lib.GPIOD_CTXLESS_EVENT_CB_RET_OK
-                    if evtype == self.lib.GPIOD_CTXLESS_EVENT_CB_RISING_EDGE:
-                        print("Rising  edge timestamp %s" % time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(ts.tv_sec)))
-                    else:
-                        print("Falling edge timestamp %s" % time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(ts.tv_sec)))
-            except:
-                rc = self.lib.GPIOD_CTXLESS_EVENT_CB_RET_ERR
-                print("Unexpected error:", sys.exc_info()[0])
-            return rc
+    def show_event(self, event):
+        if event.type == gpiod.LineEvent.RISING_EDGE:
+            print("Rising  edge timestamp %s" % time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(event.sec)))
+        elif event.type == gpiod.LineEvent.FALLING_EDGE:
+            print("Falling edge timestamp %s" % time.strftime('%m/%d/%Y %H:%M:%S', time.localtime(event.sec)))
+        else:
+            raise TypeError('Invalid event type')
 
-        # Expose callback to object instance
-        self.buttonCallback = buttonCallback
-
-    def main(self, chip, line):
+    def main(self, line):
         """Print edge events for 10 seconds.
         """         
-        print ("libgpiod version %s" % self.ffi.string(self.lib.gpiod_version_string()).decode('utf-8'))
-        # Consumer is script name without .py
-        consumer = sys.argv[0][:-3]
-        # 10 second timeout
-        timespec = self.ffi.new("struct timespec*")
-        timespec.tv_sec = 10
+        print("Name: %s, label: %s, lines: %d" % (self.chip.name(), self.chip.label(), self.chip.num_lines()))
+        line = self.chip.get_line(line)
+        line.request(consumer=sys.argv[0][:-3], type=gpiod.LINE_REQ_EV_BOTH_EDGES)
         print("Press and release button, timeout in 10 seconds\n")
-        # Blocking poll until timeout, note gpiod_simple_event_poll_cb is passed as a NULL
-        if self.lib.gpiod_ctxless_event_loop(chip.encode('utf-8'), line, False, consumer.encode('utf-8'), timespec, self.ffi.NULL, self.buttonCallback, self.ffi.NULL) != 0:
-            print("gpiod_ctxless_event_loop error, check --chip and --line values")     
+        ev_line = line.event_wait(sec=5)
+        event = ev_line.event_read()
+        self.show_event(event)
 
 
 if __name__ == "__main__":
@@ -66,5 +45,5 @@ if __name__ == "__main__":
     parser.add_argument("--chip", help="GPIO chip name (default '/dev/gpiochip1')", type=str, default="/dev/gpiochip1")
     parser.add_argument("--line", help="GPIO line number (default 3 button on NanoPi Duo)", type=int, default=3)
     args = parser.parse_args()
-    obj = buttoncallback()
-    obj.main(args.chip, args.line)
+    obj = buttoncallback(args.chip)
+    obj.main(args.line)
