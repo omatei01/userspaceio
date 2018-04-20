170a492b1abf49f33787edb84e14d1b37f765ac4
sgoldsmith <sgoldsmith@dx4350>
2018-04-19 22:51:22 -0400

    Added second chip for led

--- libgpiod/python/src/buttonpress.py
+++ libgpiod/python/src/buttonpress.py
@@ -15,10 +15,14 @@ from argparse import *
 
 class buttonpress:
     
-    def __init__(self, chip):
-        """Create library and ffi interfaces.
+    def __init__(self, chip_button, chip_led):
+        """Handle possibility of button and led on different GPIO chips
         """         
-        self.chip = gpiod.Chip(chip, gpiod.Chip.OPEN_BY_PATH)
+        self.chip_button = gpiod.Chip(chip_button, gpiod.Chip.OPEN_BY_PATH)
+        if chip_led != chip_button:
+            self.chip_led = gpiod.Chip(chip_led, gpiod.Chip.OPEN_BY_PATH)
+        else:
+            self.chip_led = self.chip_button
 
     def show_event(self, event):
         if event.type == gpiod.LineEvent.RISING_EDGE:
@@ -31,11 +35,11 @@ class buttonpress:
     def main(self, button, led):
         """Print edge events for 10 seconds.
         """         
-        print("Name: %s, label: %s, lines: %d" % (self.chip.name(), self.chip.label(), self.chip.num_lines()))
-        button_line = self.chip.get_line(button)
+        print("Name: %s, label: %s, lines: %d" % (self.chip_button.name(), self.chip_button.label(), self.chip_button.num_lines()))
+        button_line = self.chip_button.get_line(button)
         button_line.request(consumer=sys.argv[0][:-3], type=gpiod.LINE_REQ_EV_BOTH_EDGES)
         if led:
-            led_line = self.chip.get_line(led)
+            led_line = self.chip_led.get_line(led)
             led_line.request(consumer=sys.argv[0][:-3], type=gpiod.LINE_REQ_DIR_OUT)
         print("Press and release button, timeout in 10 seconds\n")
         while button_line.event_wait(sec=10):
@@ -51,8 +55,9 @@ class buttonpress:
 
 if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument("--chip", help="GPIO chip name (default '/dev/gpiochip1')", type=str, default="/dev/gpiochip1")
+    parser.add_argument("--chip_button", help="GPIO chip name (default '/dev/gpiochip1')", type=str, default="/dev/gpiochip1")
     parser.add_argument("--button", help="GPIO line number (default 3 button on NanoPi Duo)", type=int, default=3)
+    parser.add_argument("--chip_led", help="GPIO chip name (default '/dev/gpiochip0')", type=str, default="/dev/gpiochip0")
     parser.add_argument("--led", help="GPIO line number (default 203 IOG11 on NanoPi Duo)", type=int, default=203)
     args = parser.parse_args()
     obj = buttonpress(args.chip)
